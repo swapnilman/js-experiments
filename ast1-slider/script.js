19e833b8e8684261acdb991ea44037333bb6a790
swapnilman <swapnilman4@gmail.com>
2018-11-30 09:05:47 +0545

    ast1-slider: add dots to slider

--- ast1-slider/script.js
+++ ast1-slider/script.js
@@ -1,19 +1,16 @@
-function Slider(elementId) {
+function Slider(sliderId, wrapperId) {
   var that = this;
-  var element = document.getElementById(elementId);
+  var slider = document.getElementById(sliderId);
+  var wrapper = document.getElementById(wrapperId)
 
-  var img = element.getElementsByTagName('img');
-  element.appendChild(img[0]);
-
-  console.log(img);
-
+  var img = slider.getElementsByTagName('img');
   for(var i = 0; i < img.length; i++) {
     img[i].style.left = i * 800 + 'px';
   }
 
   var prev = document.getElementById("prev");
   var next = document.getElementById("next");
-  
+
   var interval;
   this.current = 0;
   this.marginLeft = 0;
@@ -21,6 +18,7 @@ function Slider(elementId) {
 
   this.init = function () {
     this.autoSlide();
+    this.createButton();
     this.listenClick();
   }
 
@@ -33,7 +31,7 @@ function Slider(elementId) {
   this.slide = function (slideSpeed) {
     interval = setInterval(function () {
       that.marginLeft -= slideSpeed;
-      element.style.marginLeft = that.marginLeft + 'px';
+      slider.style.marginLeft = that.marginLeft + 'px';
 
       if (that.marginLeft < -4000) {
         that.marginLeft = 800;
@@ -53,7 +51,7 @@ function Slider(elementId) {
     prev.addEventListener('click', that.previous);
     next.addEventListener('click', that.next);
   }
-  
+
   this.previous = function () {
 
     that.slide(-50);
@@ -69,7 +67,7 @@ function Slider(elementId) {
     // }
 
     // that.marginLeft = value;
-    // element.style.marginLeft = that.marginLeft + 'px';
+    // slider.style.marginLeft = that.marginLeft + 'px';
   }
 
   this.next = function () {
@@ -87,7 +85,72 @@ function Slider(elementId) {
     // }
 
     // that.marginLeft = value;
-    // element.style.marginLeft = that.marginLeft + 'px';
+    // slider.style.marginLeft = that.marginLeft + 'px';
   }
-}
 
+  this.createButton = function () {
+    console.log(img)
+    console.log(wrapper)
+    for (var i = 1; i <= img.length; i++) {
+      var btn = document.createElement("button");
+      btn.id = "btn" + i;
+      btn.style.border = "1px solid black";
+      btn.style.borderRadius = "10px";
+      btn.style.height = "15px";
+      btn.style.width = "15px";
+      btn.style.zIndex = 5;
+
+      wrapper.appendChild(btn);
+
+      // btn.addEventListener('click', function (i) {
+      //   return function () {
+      //     that.goto(i);
+      //     that.changeColor(i);
+      //
+      //   }
+      // }(i));
+
+      // this.reset = function () {
+      //   btn1.style.backgroundColor = '';
+      //   btn2.style.backgroundColor = '';
+      //   btn3.style.backgroundColor = '';
+      //   btn4.style.backgroundColor = '';
+      //   btn5.style.backgroundColor = '';
+      // }
+
+      // this.changeColor = function (i) {
+      //   that.reset();
+      //   var value = that.marginLeft;
+      //   var quotient = value / 1024;
+      //   console.log('quotient' + (1 - quotient));
+      //   var b = document.querySelector("#btn" + (1 - quotient));
+      //   if (that.marginLeft == 0) {
+      //     b.setAttribute("class", "active");
+      //     b.style.border = "1px solid black";
+      //     b.style.backgroundColor = "#8E44AD  ";
+      //   }
+      //   if (that.marginLeft == -1024) {
+      //     b.setAttribute("class", "active");
+      //     b.style.border = "1px solid black";
+      //     b.style.backgroundColor = "#8E44AD  ";
+      //   }
+      //   if (that.marginLeft == -2048) {
+      //     b.setAttribute("class", "active");
+      //     b.style.border = "1px solid black";
+      //     b.style.backgroundColor = "#8E44AD  ";
+      //   }
+      //   if (that.marginLeft == -3072) {
+      //     b.setAttribute("class", "active");
+      //     b.style.border = "1px solid black";
+      //     b.style.backgroundColor = "#8E44AD  ";
+      //   }
+      //   if (that.marginLeft == -4096) {
+      //     b.setAttribute("class", "active");
+      //     b.style.border = "1px solid black";
+      //     b.style.backgroundColor = "#8E44AD  ";
+      //   }
+      // };
+      // clearInterval(intervalId);
+    }
+}
+}
