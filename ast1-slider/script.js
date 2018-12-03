3e35024502f7c6d2422db981e5d59cc26fc5c4d5
swapnilman <swapnilman4@gmail.com>
2018-12-03 12:09:23 +0545

    ast1-slider: change style.css

--- ast1-slider/script.js
+++ ast1-slider/script.js
@@ -1,15 +1,35 @@
-function Slider(sliderId, wrapperId) {
+function Slider(wrapperId, containerId) {
   var that = this;
-  var slider = document.getElementById(sliderId);
-  var wrapper = document.getElementById(wrapperId)
+  var wrapper = document.getElementById(wrapperId);
+  var container = document.getElementById(containerId);
+  var img = wrapper.getElementsByTagName('img');
 
-  var img = slider.getElementsByTagName('img');
   for(var i = 0; i < img.length; i++) {
     img[i].style.left = i * 800 + 'px';
   }
 
-  var prev = document.getElementById("prev");
-  var next = document.getElementById("next");
+  var prev = document.createElement("button");
+  prev.className = 'prev';
+  prev.innerHTML = '<';
+
+  var next = document.createElement("button");
+  next.className = 'next';
+  next.innerHTML = '>';
+
+  container.appendChild(prev);
+  container.appendChild(next);
+
+  var indexBtnWrapper = document.createElement("div");
+  indexBtnWrapper.className = 'index-btn';
+
+  for (var i = 1; i <= img.length; i++) {
+    var btn = document.createElement("button");
+    btn.classList.add('indexBtn')
+    btn.id = "btn" + i;
+    indexBtnWrapper.appendChild(btn)
+  }
+
+  container.appendChild(indexBtnWrapper);
 
   var interval;
   this.current = 0;
@@ -18,7 +38,6 @@ function Slider(sliderId, wrapperId) {
 
   this.init = function () {
     this.autoSlide();
-    this.createButton();
     this.listenClick();
   }
 
@@ -31,18 +50,16 @@ function Slider(sliderId, wrapperId) {
   this.slide = function (slideSpeed) {
     interval = setInterval(function () {
       that.marginLeft -= slideSpeed;
-      slider.style.marginLeft = that.marginLeft + 'px';
+      wrapper.style.marginLeft = that.marginLeft + 'px';
 
-      if (that.marginLeft < -4000) {
-        that.marginLeft = 800;
-      }
-
-      if (that.marginLeft > 0) {
-        that.marginLeft  = -4800;
+      if (!(that.marginLeft > -4000)) {
+        that.marginLeft = 0;
+        console.log(that.marginLeft)
       }
 
       if (that.marginLeft % 800 == 0) {
         clearInterval(interval);
+        console.log(slideSpeed, that.marginLeft);
       }
     }, 1000 / 60);
   }
@@ -53,104 +70,12 @@ function Slider(sliderId, wrapperId) {
   }
 
   this.previous = function () {
-
+    clearInterval(interval);
     that.slide(-50);
-    console.log(that.marginLeft);
-
-    // var value = that.marginLeft;
-    // var remainder = value % 800;
-
-    // value = value - remainder + 800;
-
-    // if (value > 0) {
-    //   value = -4000;
-    // }
-
-    // that.marginLeft = value;
-    // slider.style.marginLeft = that.marginLeft + 'px';
   }
 
   this.next = function () {
-
+    clearInterval(interval);
     that.slide(50);
-    console.log(that.marginLeft);
-
-    // var value = that.marginLeft;
-    // var remainder = value % 800;
-
-    // value = value - remainder - 800;
-
-    // if (value < -4000) {
-    //   value = 0;
-    // }
-
-    // that.marginLeft = value;
-    // slider.style.marginLeft = that.marginLeft + 'px';
   }
-
-  this.createButton = function () {
-    console.log(img)
-    console.log(wrapper)
-    for (var i = 1; i <= img.length; i++) {
-      var btn = document.createElement("button");
-      btn.id = "btn" + i;
-      btn.style.border = "1px solid black";
-      btn.style.borderRadius = "10px";
-      btn.style.height = "15px";
-      btn.style.width = "15px";
-      btn.style.zIndex = 5;
-
-      wrapper.appendChild(btn);
-
-      // btn.addEventListener('click', function (i) {
-      //   return function () {
-      //     that.goto(i);
-      //     that.changeColor(i);
-      //
-      //   }
-      // }(i));
-
-      // this.reset = function () {
-      //   btn1.style.backgroundColor = '';
-      //   btn2.style.backgroundColor = '';
-      //   btn3.style.backgroundColor = '';
-      //   btn4.style.backgroundColor = '';
-      //   btn5.style.backgroundColor = '';
-      // }
-
-      // this.changeColor = function (i) {
-      //   that.reset();
-      //   var value = that.marginLeft;
-      //   var quotient = value / 1024;
-      //   console.log('quotient' + (1 - quotient));
-      //   var b = document.querySelector("#btn" + (1 - quotient));
-      //   if (that.marginLeft == 0) {
-      //     b.setAttribute("class", "active");
-      //     b.style.border = "1px solid black";
-      //     b.style.backgroundColor = "#8E44AD  ";
-      //   }
-      //   if (that.marginLeft == -1024) {
-      //     b.setAttribute("class", "active");
-      //     b.style.border = "1px solid black";
-      //     b.style.backgroundColor = "#8E44AD  ";
-      //   }
-      //   if (that.marginLeft == -2048) {
-      //     b.setAttribute("class", "active");
-      //     b.style.border = "1px solid black";
-      //     b.style.backgroundColor = "#8E44AD  ";
-      //   }
-      //   if (that.marginLeft == -3072) {
-      //     b.setAttribute("class", "active");
-      //     b.style.border = "1px solid black";
-      //     b.style.backgroundColor = "#8E44AD  ";
-      //   }
-      //   if (that.marginLeft == -4096) {
-      //     b.setAttribute("class", "active");
-      //     b.style.border = "1px solid black";
-      //     b.style.backgroundColor = "#8E44AD  ";
-      //   }
-      // };
-      // clearInterval(intervalId);
-    }
-}
 }
